e0253ad487e2b660f1bf215438612b0871bf9e23
Loading <83232326+Loading@users.noreply.github.com>
2024-11-09 17:17:00 +0100

    Update layout.tsx

--- my-app/src/app/services/layout.tsx
+++ my-app/src/app/services/layout.tsx
@@ -1,35 +1,36 @@
-import React, { ReactNode } from "react"; // Import ReactNode to define children type
-import Image from "next/image"; // Import Image component for optimization
+import React, { ReactNode } from "react";
+import Image from "next/image";
 
-// Define the type for the Layout component
 interface LayoutProps {
-  children: ReactNode; // Explicitly define the type of children
+  children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="banner banner-img_spray-foam-insulation">
-      <div className="text-overlay">
-        <h1 className="banner-service-h1 center">Spray Foam Insulation</h1>
-        <div className="banner-estimate">
-          <h3>Get a free quote today!</h3>
-          <div className="banner-estimate-btn">
-            <a href="tel:8555665340">
-              <button className="button-blue">
-                <Image
-                  src="/images/icons/phone.svg"
-                  alt="Phone Icon"
-                  width={24}
-                  height={24}
-                />
-                (855) 566-5340
-              </button>
-            </a>
-            <button className="button-yellow">GET A QUOTE</button>
+    <div>
+      <div className="banner banner-img_spray-foam-insulation">
+        <div className="text-overlay">
+          <h1 className="banner-service-h1 center">Spray Foam Insulation</h1>
+          <div className="banner-estimate">
+            <h3>Get a free quote today!</h3>
+            <div className="banner-estimate-btn">
+              <a href="tel:8555665340">
+                <button className="button-blue">
+                  <Image
+                    src="/images/icons/phone.svg"
+                    alt="Phone Icon"
+                    width={24}
+                    height={24}
+                  />
+                  (855) 566-5340
+                </button>
+              </a>
+              <button className="button-yellow">GET A QUOTE</button>
+            </div>
           </div>
         </div>
+        {/* Render children */}
       </div>
-      {/* Render children */}
       <div>{children}</div>
     </div>
   );
